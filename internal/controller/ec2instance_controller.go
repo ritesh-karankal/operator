9f0ca2d429df7e214ddc311474547caec63d138c
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-09-09 23:34:29 +0530

    feat: implement EC2 instance reconciliation

--- internal/controller/ec2instance_controller.go
+++ internal/controller/ec2instance_controller.go
@@ -18,21 +18,20 @@ package controller
 
 import (
 	"context"
-	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	// "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	computev1alpha1 "github.com/ritesh-karankal/operator/api/v1alpha1"
 )
 
 // EC2InstanceReconciler reconciles a EC2Instance object
 type EC2InstanceReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+	client.Client                 // Used to perform CRUD operations on Kubernetes resources.
+	Scheme        *runtime.Scheme // Used to map Go types to Kubernetes GroupVersionKinds and vice versa.
 }
 
 // +kubebuilder:rbac:groups=compute.cloud.com,resources=ec2instances,verbs=get;list;watch;create;update;patch;delete
@@ -49,17 +48,62 @@ type EC2InstanceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.24.1/pkg/reconcile
 func (r *EC2InstanceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	// l := log.FromContext(ctx)
+	l := log.FromContext(ctx)
 
+	l.Info("=== RECONCILE LOOP STARTED ===", "namespace", req.Namespace, "name", req.Name)
 
 	ec2Instance := &computev1.EC2Instance{}
-	r.Get(ctx, req.NamespacedName, ec2Instance)
+	if err := r.Get(ctx, req.NamespacedName, ec2Instance); err != nil {
+		if errors.IsNotFound(err) {
+			l.Info("Instance Deleted. No need to reconcile")
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
 
-	// l.Info("Reconciling EC2Instance", "Name", ec2Instance.Name)
-	fmt.Println("Update was made to the ec2 Instance resource this is why iam reconciling it\n", req.NamespacedName)
-	fmt.Printf("The ec2 instance type is %v\n", ec2Instance.Spec.InstanceType)
-	
+	if ec2Instance.Status.InstanceID != "" {
+		l.Info("Requested object already exists in Kubernetes. Not creating a new instance.", "instanceID", ec2Instance.Status.InstanceID)
+		return ctrl.Result{}, nil
+	}
 
+	l.Info("Crating new instance")
+
+	l.Info("=== ABOUT TO ADD FINALIZER ===")
+	ec2Instance.Finalizers = append(ec2Instance.Finalizers, "ec2instance.compute.cloud.com")
+	if err := r.Update(ctx, ec2Instance); err != nil {
+		l.Error(err, "Failed to add finalizer")
+		return ctrl.Result{
+			Requeue: true,
+
+		}, err
+	}
+
+	l.Info("=== FINALIZER ADDED - This update will triger a NEW reconcile loop, but current reconcile continues ===")
+
+	l.Info("=== CONTINUING WITH EC2 INSTANCE CREATION IN CURRENT RECONCILE ===")
+
+	createdInstanceInfo, err := createEc2Instance(ec2Instance)
+	if err != nil {
+		l.Error(err, "Failed to create EC2 instance")
+		return ctrl.Result{}, err
+	}
+
+	l.Info("=== ABOUT TO UPDATE STATUS - This will trigger reconcile loop again ===", "instanceID", createdInstanceInfo.InstanceID, "state", createdInstanceInfo.State)
+
+	ec2Instance.Status.InstanceID = createdInstanceInfo.InstanceID
+	ec2Instance.Status.State = createdInstanceInfo.State
+	ec2Instance.Status.PublicIP = createdInstanceInfo.PublicIP
+	ec2Instance.Status.PrivateIP = createdInstanceInfo.PrivateIP
+	ec2Instance.Status.PublicDNS = createdInstanceInfo.PublicDNS
+	ec2Instance.Status.PrivateDNS = createdInstanceInfo.PrivateDNS
+
+	err = r.Status()Update(ctx, ec2Instance)
+	if err != nil {
+		l.Error(err, "Failed to update status")
+		return ctrl.Result{}, err
+	}
+
+	l.Info("=== STATUS UPDATED - Reconcile loop will be triggered again ===")
 
 	return ctrl.Result{}, nil
 }
