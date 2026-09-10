6fa4549404c51932d258510b56ab3361f34fc55a
Ritesh Karankal <ritesh.karankal200@gmail.com>
2026-09-10 19:13:57 +0530

    refactor: simplify AWS client configuration by removing static credentials

--- internal/controller/awsClient.go
+++ internal/controller/awsClient.go
@@ -6,18 +6,14 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
 func awsClient(region string) *ec2.Client {
-	// read env variables for namespace
-	accesskeyID := os.Getenv("AWS_ACCESS_KEY")
-	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
 		config.WithRegion(region),
-		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accesskeyID, secretAccessKey, "")))
+	)
 	if err != nil {
 		fmt.Println("Error loading AWS config:", err)
 		os.Exit(1)
